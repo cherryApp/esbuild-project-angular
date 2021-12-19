5ba9d6af71bcec8da15e25b2aba572a61ea2e0f2
cherryApp <j.cserko@gmail.com>
2021-12-19 14:42:18 +0100

    Angular test

---
+++ ng-esbuild/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import template from './app.component.html';
+
+@Component({
+  selector: 'app-root',
+  template: template || '',
+  // styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Dr. Abigel Cserko';
+}
